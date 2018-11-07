2a9df5db656a5553134bfd35fb77d58bd668ca67
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-11-06 21:55:01 -0600

    Agrega API de google

--- front/js/app.js
+++ front/js/app.js
@@ -1,7 +1,34 @@
 var map;
+
 function initMap() {
+  var robotix = [
+    {lat: 19.277452, lng: -99.571786},
+    {lat: 18.277452, lng: -99.571786},
+    {lat: 17.277452, lng: -99.571786},
+    {lat: 16.277452, lng: -99.571786},
+    {lat: 15.277452, lng: -99.571786}
+  ];
+  
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: new google.maps.LatLng(robotix),
+    zoom: 15,
+    mapTypeId: 'terrain'
   });
+  
+  robotix.map((coords) => {
+    new google.maps.Marker({position: coords, map: map});
+  })
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+window.eqfeed_callback = function(results) {
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1],coords[0]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
 }
